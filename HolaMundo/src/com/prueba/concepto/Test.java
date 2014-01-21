b9d8e250842576a9e42105e761fad9e9356c789c
sergiohenaoa <sergiohenaoa@gmail.com>
2014-01-21 14:28:29 -0500

    Cambio - Sergio

--- HolaMundo/src/com/prueba/concepto/Test.java
+++ HolaMundo/src/com/prueba/concepto/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		try {
-			String holaMundo = "Hola Mundos Modificado ";
+			String holaMundo = "Hola Mundo Modificado ";
 			System.out.println(holaMundo);
 		} catch (Exception e) {
 			e.printStackTrace();
